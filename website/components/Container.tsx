37c76d98a6f99a279c2a7b71ee3d9b39fc625cf4
Salvino Madison <salvinomadison@Salvinos-MacBook-Air.local>
2026-08-07 00:17:04 +0530

    Initial commit

--- website/components/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export default function Container({ className = "", ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={`mx-auto w-full max-w-container px-6 md:px-10 ${className}`} {...props} />;
-}
